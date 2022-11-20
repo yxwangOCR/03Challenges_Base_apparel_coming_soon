4c7257ab530daefdde5eaae1280327b9e4a9562a
Yixuan WANG <wang.yixuan@live.fr>
2022-11-19 19:14:32 -0500

    Add email check function

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import ErrorIcon from "../images/icon-error.svg";
 
 const Main = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+    setError(false);
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (email.includes("@")) {
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
   return (
-    <div className='main'>
-      <h1>
-        <span>WE'RE</span> <br /> COMING <br /> SOON
-      </h1>
-      <p>
-        Hello fellow shoppers! We're currently building our new fashion store.
-        Add your email below to stay up-to-date with announcements and our
-        launch deals.
-      </p>
-      <div className='email-input'>
-        <input type='email' />
-        <input type='submit' value={">"} />
+    <>
+      <div className='main'>
+        <h1>
+          <span>WE'RE</span> <br /> COMING <br /> SOON
+        </h1>
+        <p>
+          Hello fellow shoppers! We're currently building our new fashion store.
+          Add your email below to stay up-to-date with announcements and our
+          launch deals.
+        </p>
+        <div className='email-input'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            value={email}
+            onChange={handleOnChange}
+            required
+          />
+          {error && <img src={ErrorIcon} />}
+          <input type='submit' value={">"} onClick={handleOnSubmit} />
+        </div>
+        {error && <h6 className='error'>Please provide a valid email!</h6>}
       </div>
-      <h6>Error</h6>
-    </div>
+    </>
   );
 };
 
